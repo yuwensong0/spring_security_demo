1b197a9c3d6da9979521b7f5bdc55b517f16b0a8
yws <1353228615@qq.com>
2020-03-10 23:29:00 +0800

    sql测试脚本

---
+++ springboot_security_jwt_rsa/security_authority.sql
@@ -0,0 +1,115 @@
+/*
+SQLyog Ultimate v10.00 Beta1
+MySQL - 5.5.62 : Database - security_authority
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`security_authority` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `security_authority`;
+
+/*Table structure for table `persistent_logins` */
+
+DROP TABLE IF EXISTS `persistent_logins`;
+
+CREATE TABLE `persistent_logins` (
+  `username` varchar(64) NOT NULL,
+  `series` varchar(64) NOT NULL,
+  `token` varchar(64) NOT NULL,
+  `last_used` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`series`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `persistent_logins` */
+
+insert  into `persistent_logins`(`username`,`series`,`token`,`last_used`) values ('xiaoming','OHfTvMbBfjPAHEuJWIrpfw==','rGnrRy1gCFkAJNyuw640Yg==','2020-03-08 12:41:54'),('xiaoming','veR8ht+7dAMaN4wrHMyTyw==','FmPmSJR0ITp1qpDmVIeDmQ==','2020-03-08 12:43:24');
+
+/*Table structure for table `sys_permission` */
+
+DROP TABLE IF EXISTS `sys_permission`;
+
+CREATE TABLE `sys_permission` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT COMMENT '编号',
+  `permission_NAME` varchar(30) DEFAULT NULL COMMENT '菜单名称',
+  `permission_url` varchar(100) DEFAULT NULL COMMENT '菜单地址',
+  `parent_id` int(11) NOT NULL DEFAULT '0' COMMENT '父菜单id',
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `sys_permission` */
+
+/*Table structure for table `sys_role` */
+
+DROP TABLE IF EXISTS `sys_role`;
+
+CREATE TABLE `sys_role` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT COMMENT '编号',
+  `ROLE_NAME` varchar(30) DEFAULT NULL COMMENT '角色名称',
+  `ROLE_DESC` varchar(60) DEFAULT NULL COMMENT '角色描述',
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+
+/*Data for the table `sys_role` */
+
+insert  into `sys_role`(`ID`,`ROLE_NAME`,`ROLE_DESC`) values (6,'ROLE_USER','基本角色'),(7,'ROLE_PRODUCT','产品角色');
+
+/*Table structure for table `sys_role_permission` */
+
+DROP TABLE IF EXISTS `sys_role_permission`;
+
+CREATE TABLE `sys_role_permission` (
+  `RID` int(11) NOT NULL COMMENT '角色编号',
+  `PID` int(11) NOT NULL COMMENT '权限编号',
+  PRIMARY KEY (`RID`,`PID`),
+  KEY `FK_Reference_12` (`PID`),
+  CONSTRAINT `FK_Reference_11` FOREIGN KEY (`RID`) REFERENCES `sys_role` (`ID`),
+  CONSTRAINT `FK_Reference_12` FOREIGN KEY (`PID`) REFERENCES `sys_permission` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `sys_role_permission` */
+
+/*Table structure for table `sys_user` */
+
+DROP TABLE IF EXISTS `sys_user`;
+
+CREATE TABLE `sys_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(32) NOT NULL COMMENT '用户名称',
+  `password` varchar(120) NOT NULL COMMENT '密码',
+  `status` int(1) DEFAULT '1' COMMENT '1开启0关闭',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+/*Data for the table `sys_user` */
+
+insert  into `sys_user`(`id`,`username`,`password`,`status`) values (4,'xiaoming','$2a$10$fcgIhvn0HPnnhDnn2WWtNej2ReBii0vPotQHjmDRFhWUetYlAx/mK',1),(5,'xiamma','$2a$10$wZ1NoAyKlGBu6DlkcZVqQeUQYljETiWVkr9QtACvKaryrpko4NC56',1);
+
+/*Table structure for table `sys_user_role` */
+
+DROP TABLE IF EXISTS `sys_user_role`;
+
+CREATE TABLE `sys_user_role` (
+  `UID` int(11) NOT NULL COMMENT '用户编号',
+  `RID` int(11) NOT NULL COMMENT '角色编号',
+  PRIMARY KEY (`UID`,`RID`),
+  KEY `FK_Reference_10` (`RID`),
+  CONSTRAINT `FK_Reference_10` FOREIGN KEY (`RID`) REFERENCES `sys_role` (`ID`),
+  CONSTRAINT `FK_Reference_9` FOREIGN KEY (`UID`) REFERENCES `sys_user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `sys_user_role` */
+
+insert  into `sys_user_role`(`UID`,`RID`) values (4,6),(5,6),(4,7),(5,7);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
